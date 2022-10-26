88be173b660ac68d00c073149b2eb2d556318615
Ilman Eldarov Ash-shishani <ilmaneldarov@gmail.com>
2022-10-26 21:53:13 +0300

    weq

--- src/Utils/formatEventStart.js
+++ src/Utils/formatEventStart.js
@@ -1,5 +1,5 @@
 import { format } from 'date-fns'
 
-export const formatEventStart = start => {
-    return format(Date.parse(start), 'dd MMMM yyyy , HH:mm')
+export const formatEventStart = ({ start }) => {
+    return format(Date.parse(start), 'dd MMMM y , HH:mm');
 };
--- src/components/Event/Event.jsx
+++ src/components/Event/Event.jsx
@@ -1,13 +1,19 @@
 import PropTypes from "prop-types";
+import { formatEventStart } from 'utils/formatEventStart'
 import { IoLocationSharp } from "react-icons/io5";
 import css  from "./Event.module.css";
 import { IoIosPerson } from "react-icons/io";
 import { BsClockFill } from "react-icons/bs";
 import { FaCalendarAlt } from "react-icons/fa";
 
-
+ // const formattedStart = formatEventStart(start);
 export const Event = ({ name, location, speaker, type, start, end }) => {
+    
+//   const formattedStart = formatEventStart(start);
+    // const bet = ret(start)
     return (
+
+
         <div className={css.event}>
             <h2 className={css.title}>{name}</h2>
             <p className={css.info}>
@@ -24,11 +30,12 @@ export const Event = ({ name, location, speaker, type, start, end }) => {
             </p>
             <p className={css.info}>
                 <BsClockFill className={css.icon} size={20}/>
-                Duration
+                {end}
             </p>
             {/* <span className="chip free|paid|vip">Event type</span> */}
         </div>
     );
+    
 };
 
 Event.propTypes = {
