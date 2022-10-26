f36fcfc2efce513895b5b3ee4b01216f588c8ec2
Ilman Eldarov Ash-shishani <ilmaneldarov@gmail.com>
2022-10-26 19:13:34 +0300

    wd

--- src/components/Event/Event.jsx
+++ src/components/Event/Event.jsx
@@ -1,6 +1,9 @@
 import PropTypes from "prop-types";
 import { IoLocationSharp } from "react-icons/io5";
 import css  from "./Event.module.css";
+import { IoIosPerson } from "react-icons/io";
+import { BsClockFill } from "react-icons/bs";
+import { FaCalendarAlt } from "react-icons/fa";
 
 
 export const Event = ({ name, location, speaker, type, start, end }) => {
@@ -8,19 +11,19 @@ export const Event = ({ name, location, speaker, type, start, end }) => {
         <div className={css.event}>
             <h2 className={css.title}>{name}</h2>
             <p className={css.info}>
-                <IoLocationSharp />
+                <IoLocationSharp className={css.icon} size={22} />
                 {location}
             </p>
             <p className={css.info}>
-                <i className="icon"></i>
+                <IoIosPerson className={css.icon} size={22}/>
                 {speaker}
             </p>
             <p className={css.info}>
-                <i className="icon"></i>
+                <FaCalendarAlt className={css.icon} size={20}/>
                 {start}
             </p>
             <p className={css.info}>
-                <i className="icon"></i>
+                <BsClockFill className={css.icon} size={20}/>
                 Duration
             </p>
             {/* <span className="chip free|paid|vip">Event type</span> */}
